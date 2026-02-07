86b5842e0493c4df3dcd384eaa46f7234fa5f3fa
Lalith Aditya <lalithaditya228@gmail.com>
2026-02-07 17:38:44 +0530

    fix buses route

--- backend/routes/busRoutes.js
+++ backend/routes/busRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const { searchBuses } = require("../controllers/busController");
 
 router.get("/search", searchBuses);
+router.get("/", busController.getAllBuses);
+
 
 module.exports = router;
