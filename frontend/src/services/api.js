75ac0adb6635258a9b1599c4d0821558e7944463
Lalith Aditya <lalithaditya228@gmail.com>
2026-02-07 17:21:49 +0530

    env setup

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,7 +4,6 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-// Attach token automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
 
