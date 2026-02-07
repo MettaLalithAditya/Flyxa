ead655d1abf1a1d41eda5d96de9d02de631cb055
Lalith Aditya <lalithaditya228@gmail.com>
2026-02-07 17:53:52 +0530

    improved bus search

--- backend/controllers/busController.js
+++ backend/controllers/busController.js
@@ -3,18 +3,22 @@ const Bus = require("../models/Bus");
 // SEARCH BUSES
 exports.searchBuses = async (req, res) => {
   try {
-    const { from, to } = req.query;
+    let { from, to } = req.query;
 
     if (!from || !to) {
       return res.status(400).json({ message: "From and To are required" });
     }
 
+    // trim spaces
+    from = from.trim();
+    to = to.trim();
+
     const buses = await Bus.find({
-      from: { $regex: from, $options: "i" },
-      to: { $regex: to, $options: "i" },
+      from: { $regex: `^${from}$`, $options: "i" },
+      to: { $regex: `^${to}$`, $options: "i" },
     });
 
-    res.json(buses);
+    res.status(200).json(buses);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
